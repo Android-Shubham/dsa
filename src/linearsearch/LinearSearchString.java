4a8bf17a5eea4d27373fbde5a89a4fdde5ad8765
Shubham Sharma <shubhamcodebrew@gmail.com>
2023-08-15 15:11:51 +0530

    Linear search in String

---
+++ src/linearsearch/LinearSearchString.java
@@ -0,0 +1,24 @@
+package linearsearch;
+
+public class LinearSearchString {
+    public static void main(String[] args) {
+        String str = "shubham";
+        int pos = linearSearchString(str, 'x');
+        System.out.println(pos);
+    }
+
+    private static int linearSearchString(String str, char target) {
+        if (str.length() == 0) {
+            return -1;
+        }
+
+        for (int index = 0; index < str.length(); index++) {
+            char pointer = str.charAt(index);
+            if (pointer == target) {
+                return index;
+            }
+        }
+        
+        return -1;
+    }
+}

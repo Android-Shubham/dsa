528c956dd6005d4a3dd9f4eecf416430aa059a3e
Shubham Sharma <shubham@reach52.com>
2023-08-26 13:23:49 +0530

    Bubble sort

---
+++ src/bubblesort/BubbleSort.java
@@ -0,0 +1,28 @@
+package bubblesort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = {3,2,8,9,1,0,-1,-2,10};
+        System.out.println(Arrays.toString(bubbleSort(nums)));
+    }
+
+    static int[] bubbleSort(int[] nums){
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 1; j < nums.length - i; j++) {
+                if(nums[j-1] > nums[j]){
+                    swap(nums,j-1,j);
+                }
+            }
+        }
+
+        return nums;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

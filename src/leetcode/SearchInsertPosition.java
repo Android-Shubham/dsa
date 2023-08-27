735940c96d262e2d1716875a4232dc2f239ebc5c
Shubham Sharma <shubhamcodebrew@gmail.com>
2023-08-27 08:29:20 +0530

    https://leetcode.com/problems/search-insert-position/description/

---
+++ src/leetcode/SearchInsertPosition.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+//https://leetcode.com/problems/search-insert-position/description/
+// ceil of a number
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 2;
+        System.out.println(searchInsert(nums,target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int pos = binarySearch(nums,target);
+        return pos;
+    }
+
+
+
+
+    public static int binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(target > nums[mid]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+}

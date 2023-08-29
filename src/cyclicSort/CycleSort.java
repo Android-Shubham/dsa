2ccb3cd1ef0ea2ceba4e976f010cb97d5d958f45
Shubham Sharma <shubhamcodebrew@gmail.com>
2023-08-29 08:54:05 +0530

    Cyclic sort
    When the ranges are given, apply cyclic sort

---
+++ src/cyclicSort/CycleSort.java
@@ -0,0 +1,32 @@
+package cyclicSort;
+
+import java.util.Arrays;
+
+public class CycleSort {
+    public static void main(String[] args) {
+        int[] arr = {0,3, 5, 2, 1, 4};
+        //range is given
+        //apply cyclic sort
+        System.out.println(Arrays.toString(cyclicSort(arr)));
+    }
+
+    private static int[] cyclicSort(int[] arr) {
+        int index = 0;
+        int last = arr.length;
+        while (index < last) {
+            if (index != arr[index]) {
+                swap(arr, index, arr[index]);
+            } else {
+                index++;
+            }
+        }
+        return arr;
+    }
+
+    static void swap(int[] nums, int start, int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+
+}

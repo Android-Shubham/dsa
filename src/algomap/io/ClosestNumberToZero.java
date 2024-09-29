81c16058879b1cfaf4c69296b51ed798926cea15
shubham_brews_code <shubhamcodebrew@gmail.com>
2024-09-29 21:14:26 +0530

    https://leetcode.com/problems/find-closest-number-to-zero/description/

---
+++ src/algomap/io/ClosestNumberToZero.java
@@ -0,0 +1,23 @@
+package algomap.io;
+
+//https://leetcode.com/problems/find-closest-number-to-zero/description/
+public class ClosestNumberToZero {
+    public static void main(String[] args) {
+        System.out.println(closestNumberToZero(new int[]{2,1,-1}));
+    }
+
+    private static int closestNumberToZero(int[] nums) {
+        int res = 0;
+        int min = Integer.MAX_VALUE;
+        for(int num : nums){
+            if(Math.abs(num) < min){
+                min = Math.abs(num);
+                res = num;
+            }else if(Math.abs(num) == min){
+                res = Math.max(res, num);
+            }
+
+        }
+        return res;
+    }
+}

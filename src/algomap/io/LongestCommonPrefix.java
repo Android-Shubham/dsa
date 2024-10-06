d5d52c93f161c734df379f060919e3c93bcda9cb
shubham_brews_code <shubhamcodebrew@gmail.com>
2024-10-06 13:41:25 +0530

    https://leetcode.com/problems/longest-common-prefix/description/

--- src/algomap/io/LongestCommonPrefix.java
+++ src/algomap/io/LongestCommonPrefix.java
@@ -1,27 +1,27 @@
 package algomap.io;
 
 import java.util.Arrays;
-import java.util.List;
 
 //https://leetcode.com/problems/longest-common-prefix/description/
 public class LongestCommonPrefix {
 
     public static void main(String[] args) {
         LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
-        System.out.println(longestCommonPrefix.longestCommonPrefix(new String[]{"abc","flower","fl","flow","flight","zing"}));
+        System.out.println(longestCommonPrefix.longestCommonPrefix(new String[]{"abc", "flower", "fl", "flow", "flight", "zing"}));
     }
+
     public String longestCommonPrefix(String[] strs) {
         Arrays.sort(strs);
         String first = strs[0];
-        String last = strs[1];
+        String last = strs[strs.length - 1];
         int c = 0;
-        while(c<first.length()){
-            if(first.charAt(c)==last.charAt(c)){
+        while (c < first.length()) {
+            if (first.charAt(c) == last.charAt(c)) {
                 c++;
-            }else {
+            } else {
                 break;
             }
         }
-        return c==0?"":first.substring(0,c);
+        return c == 0 ? "" : first.substring(0, c);
     }
 }

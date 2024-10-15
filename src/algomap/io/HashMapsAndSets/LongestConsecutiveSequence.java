a2e5103c1868839a3951ae38a68acf1964ef7df8
shubham_brews_code <shubhamcodebrew@gmail.com>
2024-10-15 22:22:32 +0530

    Longest consecutive sequence

---
+++ src/algomap/io/HashMapsAndSets/LongestConsecutiveSequence.java
@@ -0,0 +1,35 @@
+package algomap.io.HashMapsAndSets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+
+    public static void main(String[] args) {
+        LongestConsecutiveSequence longestConsecutiveSequence = new LongestConsecutiveSequence();
+        System.out.println(longestConsecutiveSequence.longestConsecutive(new int[]{-1, 1, 0}));
+    }
+
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+        for (int num : nums) {
+            if (!set.contains(num - 1)) { // Check if it's the start of a sequence
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);}
+        }
+
+        return longestStreak;
+    }
+}

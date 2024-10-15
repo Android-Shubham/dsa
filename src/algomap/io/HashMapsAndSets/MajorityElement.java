ab07c9d68667f30cfb86345d4c28f7565846c6c4
shubham_brews_code <shubhamcodebrew@gmail.com>
2024-10-15 12:47:34 +0530

    https://leetcode.com/problems/majority-element/

--- src/algomap/io/HashMapsAndSets/MajorityElement.java
+++ src/algomap/io/HashMapsAndSets/MajorityElement.java
@@ -9,7 +9,23 @@ public class MajorityElement {
         MajorityElement majorityElement = new MajorityElement();
         System.out.println(majorityElement.majorityElement(new int[]{1,2,2}));
     }
+
     public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+    public int majorityElement_bruteForce(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length / 2];
     }

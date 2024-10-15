4f7535cf07ffb6c411ab027b4fafbe09de38c0eb
shubham_brews_code <shubhamcodebrew@gmail.com>
2024-10-15 10:00:20 +0530

    https://leetcode.com/problems/separate-black-and-white-balls

--- src/leetcode/SeparateBlackAndWhite.java
+++ src/leetcode/SeparateBlackAndWhite.java
@@ -10,7 +10,8 @@ public class SeparateBlackAndWhite {
     public long minimumSteps(String s) {
         int black = 0;
         long steps = 0;
-        for (int i = 0; i < s.length(); i++) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
             if(s.charAt(i)=='0'){
                 steps += (i-black);
                 black++;

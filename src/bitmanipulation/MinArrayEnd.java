55648fc029032ae59d02bf511b2052aa18ccaa89
shubham_brews_code <shubhamcodebrew@gmail.com>
2024-11-09 17:41:58 +0530

    Min Array end

---
+++ src/bitmanipulation/MinArrayEnd.java
@@ -0,0 +1,11 @@
+package bitmanipulation;
+
+public class MinArrayEnd {
+    public long minEnd(int n, int x) {
+        long result = x;
+        while(--n>0){
+            result = (result+1)|x;
+        }
+        return result;
+    }
+}

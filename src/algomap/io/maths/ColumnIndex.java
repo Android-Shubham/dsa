ca06f905e1285f2c0fa1fc3a41dafcf311bc38db
shubham_brews_code <shubhamcodebrew@gmail.com>
2024-11-26 08:49:49 +0530

    https://leetcode.com/problems/excel-sheet-column-number/description/

---
+++ src/algomap/io/maths/ColumnIndex.java
@@ -0,0 +1,11 @@
+package algomap.io.maths;
+
+public class ColumnIndex {
+    public int titleToNumber(String columnTitle) {
+        int ans = 0;
+        for(char ch : columnTitle.toCharArray()){
+            ans = (ans*26) +(ch - 'A' + 1);
+        }
+        return ans;
+    }
+}

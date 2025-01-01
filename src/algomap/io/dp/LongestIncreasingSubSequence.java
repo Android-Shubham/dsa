63e33c88fd43fbe325c83754916a2efb36d76387
shubham_brews_code <shubhamcodebrew@gmail.com>
2025-01-01 10:20:14 +0530

    https://leetcode.com/problems/longest-increasing-subsequence/description/

---
+++ src/algomap/io/dp/LongestIncreasingSubSequence.java
@@ -0,0 +1,20 @@
+package algomap.io.dp;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubSequence {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = 1;
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max,dp[i]);
+                }
+            }
+        }
+        return max;
+    }
+}

27e9e8f22d8033d818a06e0015baff00ff2251d5
shubham_brews_code <shubhamcodebrew@gmail.com>
2025-01-20 13:23:52 +0530

    https://leetcode.com/problems/first-completely-painted-row-or-column/description/?envType=daily-question&envId=2025-01-20

---
+++ src/leetcode/PaintedRowOrCol.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class PaintedRowOrCol {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        HashMap<Integer,int[]> map = new HashMap<>();
+        int n = mat.length;
+        int m = mat[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                map.put(mat[i][j],new int[]{i,j});
+            }
+        }
+        for(int i=0;i<m*n;i++){
+            int[] cur = map.get(arr[i]);
+            mat[cur[0]][cur[1]] = -arr[i];
+            if(checkRow(cur[0],m,mat) || checkCol(cur[1],n,mat)){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private boolean checkRow(int i,int m,int[][] mat){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkCol(int j,int n,int[][] mat){
+        for(int i=0;i<n;i++){
+            if(mat[i][j]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

fb046662224f60193878508a563a437c5200661d
shubham_brews_code <shubhamcodebrew@gmail.com>
2025-01-22 16:18:46 +0530

    https://leetcode.com/problems/special-positions-in-a-binary-matrix/

---
+++ src/leetcode/SpecialPositionInBinaryMatrix.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class SpecialPositionInBinaryMatrix {
+    public int numSpecial(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[] rows = new int[n];
+        int[] cols = new int[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                rows[i]+=mat[i][j];
+                cols[j]+=mat[i][j];
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1 && rows[i]==1 && cols[j]==1){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}

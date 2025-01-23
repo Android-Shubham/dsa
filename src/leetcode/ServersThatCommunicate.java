4008d97e76627875adadfb3f6e39c725b9912bd3
shubham_brews_code <shubhamcodebrew@gmail.com>
2025-01-23 16:19:43 +0530

    https://leetcode.com/problems/count-servers-that-communicate/description/?envType=daily-question&envId=2025-01-23

--- src/leetcode/ServersThatCommunicate.java
+++ src/leetcode/ServersThatCommunicate.java
@@ -1,7 +1,31 @@
 package leetcode;
 
 public class ServersThatCommunicate {
+
     public int countServers(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] row = new int[n];
+        int[] col = new int[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public int countServers_bruteforce(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         boolean[][] marked = new boolean[n][m];

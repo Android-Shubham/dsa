ec4c32eb2485d506b4679c2336bad9f3ac2eb037
shubham_brews_code <shubhamcodebrew@gmail.com>
2025-01-26 09:59:59 +0530

    https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/

---
+++ src/leetcode/GarbageCollection.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class GarbageCollection {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int last_p = -1;
+        int last_g = -1;
+        int last_m = -1;
+        int total = 0;
+        for(int i=1;i<travel.length;i++){
+            travel[i]+=travel[i-1];
+        }
+        for(int i=0;i<garbage.length;i++){
+            String g = garbage[i];
+            total+=g.length();
+            if(g.contains("G")){
+                last_g = i;
+            }
+            if(g.contains("P")){
+                last_p = i;
+            }
+            if(g.contains("M")){
+                last_m = i;
+            }
+        }
+        if(last_p!=-1){
+            total+=(travel[last_p-1]);
+        }
+        if(last_g>0){
+            total+=(travel[last_g-1]);
+        }
+        if(last_m>0){
+            total+=(travel[last_m-1]);
+        }
+        return total;
+    }
+}

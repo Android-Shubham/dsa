bc6be8aefa337b68c3e4d783e052595ff524f261
shubham_brews_code <shubhamcodebrew@gmail.com>
2025-04-02 09:51:12 +0530

    https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/?envType=daily-question&envId=2025-04-02

--- src/leetcode/MaxValueOfAnOrderedTriplet.java
+++ src/leetcode/MaxValueOfAnOrderedTriplet.java
@@ -1,15 +1,23 @@
 package leetcode;
 
 public class MaxValueOfAnOrderedTriplet {
+    public static void main(String[] args) {
+        MaxValueOfAnOrderedTriplet max = new MaxValueOfAnOrderedTriplet();
+        System.out.println(max.maximumTripletValue(new int[]{12, 6, 1, 2, 7}));
+    }
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = nums[0];
+        right[n-1] = nums[n-1];
+        for(int i=1;i<n;i++){
+            left[i] = Math.max(left[i-1],nums[i]);
+            right[n-i-1] = Math.max(right[n-i],nums[n-i-1]);
+        }
         long res = 0;
-        for(int k=2;k<n;k++){
-            int max = nums[0];
-            for(int j=1;j<k;j++){
-                res = Math.max(res,(long)(max-nums[j])*nums[k]);
-                max = Math.max(max,nums[j]);
-            }
+        for(int j=1;j<n-1;j++){
+            res = Math.max(res,(long)(left[j-1] - nums[j])*right[j+1]);
         }
         return res;
     }

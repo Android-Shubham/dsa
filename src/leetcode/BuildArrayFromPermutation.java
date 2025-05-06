5d2cb6e07e3ce117b06f10bb4065c0f316e4def4
shubham_brews_code <shubhamcodebrew@gmail.com>
2025-05-06 09:33:18 +0530

    Build array from permutation

---
+++ src/leetcode/BuildArrayFromPermutation.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            nums[i]+=(1024 * (nums[nums[i]]%1024));
+        }
+        for(int i=0;i<n;i++){
+            nums[i]/=1024;
+        }
+        return nums;
+    }
+}
